d7818967bb74b314830cfae7ccb5dcd48a6fe011
DilipGupta <dilipguptaiq@gmail.com>
2020-08-30 19:01:16 -0700

    Update best_answer_id on question table when an answer removed using Eloquent event

--- app/Answer.php
+++ app/Answer.php
@@ -31,11 +31,23 @@ class Answer extends Model
         });
 
         static::deleted(function($answer){
-            $answer->question->decrement('answers_count');
+            $question=$answer->question;
+            
+            $question->decrement('answers_count');
+
+            if($question->best_answer_id === $answer->id){
+                $question->best_answer_id = Null;
+                $question->save();
+            }
         });
     }
 
     public function getCreatedDateAttribute(){
         return $this->created_at->diffForHumans();
+    }
+    
+    //This accessor tell best answer status
+    public function getStatusAttribute(){
+        return $this->id === $this->question->best_answer_id ? 'vote-accepted' : '';
     }
 }
